be758b351d2ba7f82f21b9bbc79c7fa57cbde86a
uyab <uyab.exe@gmail.com>
2015-06-10 17:23:43 +0700

    prepare download link untuk aplikasi android

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,8 @@
         <div style="text-align: center">
             <h3>Silakan pilih menu</h3>
             <a class="button button-primary" href="{{ url('symptom') }}">ISDA Online</a>
-            <a class="button button-primary" href="{{ url('diagnose') }}">NANDA Editor</a>
+            <a class="button" href="{{ url('diagnose') }}">NANDA Editor</a>
+            <a class="button" href="{{ url('download/isda.apk') }}">Download Aplikasi Android</a>
         </div>
     </div>
 @endsection
