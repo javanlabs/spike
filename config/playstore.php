70730428f2d03b061905d88c762d590d70db0caf
Ricky D <ricky@ngomik.com>
2015-10-02 09:52:46 +0700

    add new product id.

--- config/playstore.php
+++ config/playstore.php
@@ -9,7 +9,6 @@
 return [
 
     'public_key' => 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiI5djt4rB2ihWPR9p7XSI3QtLVbu+6AO5zfQUvmSo3PaM2lqP5d0LZWifsj1P/6AMnkBbCqpgS+SQCIe985qwLIdJ4rd/CFQMPbxofFGMrGNaMAO64O/WnPQGixAiePCgCnWdduzh3OFvOGdjSkj1eLAqChMk2hHNQx9xZVmiq2/pfGdUZ9DpX1iqwMc2T/S/q6lTbnzQoZ5XkaeG4sNfL5HvvuOYJ0kOO06+9vKnzUebe+WtFkNOEA+k87GIdZAMkIe1u0Wy4w+4DC9iSMtyfqXHmDXySpP03fxVvBlhuTat8EWJ19aGNEKUTbv7x16ridyTobqJOJNMidxUvhERwIDAQAB',
-
-    'productId' => ['com.javanlabs.isda.test_monthly']
-
+    
+    'productId' => ['com.javanlabs.isda.test_monthly', 'com.javanlabs.isda.akses_bulanan']
 ];
